2540966504e0e82310ac20f08599f35d47dfff92
jiaengoh <57712490+jiaengoh@users.noreply.github.com>
2022-12-03 16:42:07 +0800

    feature/show-dynamic-username

--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -1,9 +1,11 @@
 import { useState, useEffect } from "react";
 import Form from "react-bootstrap/Form";
 import { useNavigate, Outlet } from "react-router-dom";
+import AuthService from "../services/auth";
 
 const Home = () => {
   const [account, setAccount] = useState("saving");
+  const currentUser = AuthService.getCurrentUser();
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -13,7 +15,7 @@ const Home = () => {
   return (
     <div>
       <div style={{ marginTop: 40, display: "flex" }}>
-        <h1 style={{ width: "70%" }}>Welcome Vance</h1>
+        <h1 style={{ width: "70%" }}>Welcome {currentUser.Username}</h1>
         <Form.Select
           aria-label="accounts"
           value={account}
