742ff75767ebf92275c52c23e62d8c3461d16281
travis <traviscanbefound@gmail.com>
2022-12-03 15:37:42 +0800

    nit: split props around

--- frontend/src/components/TransactionsTable.js
+++ frontend/src/components/TransactionsTable.js
@@ -8,148 +8,148 @@ import "jspdf-autotable";
 import Button from "react-bootstrap/Button";
 import DeleteModal from "./DeleteModal";
 
-export const TransactionTable = ({ typeState }) => {
-  // Current dummy data
-  const dummyData = [
-    {
-      TransactionID: 1,
-      AccountID: 621156213,
-      ReceivingAccountID: 339657462,
-      Date: "2022-11-08T04:00:00.000Z",
-      TransactionAmount: 500.0,
-      Comment: "Monthly Pocket Money",
-    },
-    {
-      TransactionID: 2,
-      AccountID: 958945214,
-      ReceivingAccountID: 621156213,
-      Date: "2022-11-08T04:00:00.000Z",
-      TransactionAmount: 8996.0,
-      Comment: "School Fees",
-    },
-    {
-      TransactionID: 3,
-      AccountID: 828120424,
-      ReceivingAccountID: 322798030,
-      Date: "2022-11-25T04:00:00.000Z",
-      TransactionAmount: 3000.0,
-      Comment: "Driving Centre Top-up",
-    },
-    {
-      TransactionID: 4,
-      AccountID: 353677039,
-      ReceivingAccountID: 785703027,
-      Date: "2022-11-17T06:21:00.000Z",
-      TransactionAmount: 255.0,
-      Comment: "Tuition Fee Payment",
-    },
-    {
-      TransactionID: 5,
-      AccountID: 259555772,
-      ReceivingAccountID: 828120424,
-      Date: "2022-11-08T04:00:00.000Z",
-      TransactionAmount: 32.0,
-      Comment: "Books Payment",
-    },
-  ];
+export const TransactionTable = ({ transactions }) => {
+  //   // Current dummy data
+  //   const dummyData = [
+  //     {
+  //       TransactionID: 1,
+  //       AccountID: 621156213,
+  //       ReceivingAccountID: 339657462,
+  //       Date: "2022-11-08T04:00:00.000Z",
+  //       TransactionAmount: 500.0,
+  //       Comment: "Monthly Pocket Money",
+  //     },
+  //     {
+  //       TransactionID: 2,
+  //       AccountID: 958945214,
+  //       ReceivingAccountID: 621156213,
+  //       Date: "2022-11-08T04:00:00.000Z",
+  //       TransactionAmount: 8996.0,
+  //       Comment: "School Fees",
+  //     },
+  //     {
+  //       TransactionID: 3,
+  //       AccountID: 828120424,
+  //       ReceivingAccountID: 322798030,
+  //       Date: "2022-11-25T04:00:00.000Z",
+  //       TransactionAmount: 3000.0,
+  //       Comment: "Driving Centre Top-up",
+  //     },
+  //     {
+  //       TransactionID: 4,
+  //       AccountID: 353677039,
+  //       ReceivingAccountID: 785703027,
+  //       Date: "2022-11-17T06:21:00.000Z",
+  //       TransactionAmount: 255.0,
+  //       Comment: "Tuition Fee Payment",
+  //     },
+  //     {
+  //       TransactionID: 5,
+  //       AccountID: 259555772,
+  //       ReceivingAccountID: 828120424,
+  //       Date: "2022-11-08T04:00:00.000Z",
+  //       TransactionAmount: 32.0,
+  //       Comment: "Books Payment",
+  //     },
+  //   ];
 
-  // Dummy account data split on account type
-  const dummySplitAccountData = [
-    {
-      AccountID: 621156213,
-      UserID: 1,
-      AccountType: "Saving",
-      AcccountBalance: 70200.71,
-    },
-    {
-      AccountID: 958945214,
-      UserID: 1,
-      AccountType: "Current",
-      AcccountBalance: 99720.46,
-    },
-    {
-      AccountID: 828120424,
-      UserID: 2,
-      AccountType: "Multiplier",
-      AcccountBalance: 50640.12,
-    },
-    {
-      AccountID: 322798030,
-      UserID: 3,
-      AccountType: "Multiplier",
-      AcccountBalance: 39740.17,
-    },
-    {
-      AccountID: 353677039,
-      UserID: 3,
-      AccountType: "Saving",
-      AcccountBalance: 76660.21,
-    },
-    {
-      AccountID: 259555772,
-      UserID: 4,
-      AccountType: "Saving",
-      AcccountBalance: 14020.58,
-    },
-    {
-      AccountID: 339657462,
-      UserID: 1,
-      AccountType: "Current",
-      AcccountBalance: 47380.33,
-    },
-    {
-      AccountID: 785703027,
-      UserID: 5,
-      AccountType: "Current",
-      AcccountBalance: 42460.32,
-    },
-  ];
+  //   // Dummy account data split on account type
+  //   const dummySplitAccountData = [
+  //     {
+  //       AccountID: 621156213,
+  //       UserID: 1,
+  //       AccountType: "Saving",
+  //       AcccountBalance: 70200.71,
+  //     },
+  //     {
+  //       AccountID: 958945214,
+  //       UserID: 1,
+  //       AccountType: "Current",
+  //       AcccountBalance: 99720.46,
+  //     },
+  //     {
+  //       AccountID: 828120424,
+  //       UserID: 2,
+  //       AccountType: "Multiplier",
+  //       AcccountBalance: 50640.12,
+  //     },
+  //     {
+  //       AccountID: 322798030,
+  //       UserID: 3,
+  //       AccountType: "Multiplier",
+  //       AcccountBalance: 39740.17,
+  //     },
+  //     {
+  //       AccountID: 353677039,
+  //       UserID: 3,
+  //       AccountType: "Saving",
+  //       AcccountBalance: 76660.21,
+  //     },
+  //     {
+  //       AccountID: 259555772,
+  //       UserID: 4,
+  //       AccountType: "Saving",
+  //       AcccountBalance: 14020.58,
+  //     },
+  //     {
+  //       AccountID: 339657462,
+  //       UserID: 1,
+  //       AccountType: "Current",
+  //       AcccountBalance: 47380.33,
+  //     },
+  //     {
+  //       AccountID: 785703027,
+  //       UserID: 5,
+  //       AccountType: "Current",
+  //       AcccountBalance: 42460.32,
+  //     },
+  //   ];
 
-  const accountIds = [];
+  //   const accountIds = [];
 
-  const checkAccountType = (typeState) => {
-    if (typeState === "Saving") {
-      dummySplitAccountData.map((account) => {
-        if (account.AccountType === "Saving") {
-          accountIds.push(account.AccountID);
-        }
-      });
-    } else if (typeState === "Current") {
-      dummySplitAccountData.map((account) => {
-        if (account.AccountType === "Current") {
-          accountIds.push(account.AccountID);
-        }
-      });
-    } else if (typeState === "Multiplier") {
-      dummySplitAccountData.map((account) => {
-        if (account.AccountType === "Multiplier") {
-          accountIds.push(account.AccountID);
-        }
-      });
-    }
-  };
+  //   const checkAccountType = (typeState) => {
+  //     if (typeState === "Saving") {
+  //       dummySplitAccountData.map((account) => {
+  //         if (account.AccountType === "Saving") {
+  //           accountIds.push(account.AccountID);
+  //         }
+  //       });
+  //     } else if (typeState === "Current") {
+  //       dummySplitAccountData.map((account) => {
+  //         if (account.AccountType === "Current") {
+  //           accountIds.push(account.AccountID);
+  //         }
+  //       });
+  //     } else if (typeState === "Multiplier") {
+  //       dummySplitAccountData.map((account) => {
+  //         if (account.AccountType === "Multiplier") {
+  //           accountIds.push(account.AccountID);
+  //         }
+  //       });
+  //     }
+  //   };
 
-  checkAccountType(typeState);
+  //   checkAccountType(typeState);
 
-  const filteredDummyData = [];
+  //   const filteredDummyData = [];
 
-  dummyData.map((transaction) => {
-    if (accountIds.includes(transaction.AccountID)) {
-      filteredDummyData.push(transaction);
-    }
-  });
+  //   dummyData.map((transaction) => {
+  //     if (accountIds.includes(transaction.AccountID)) {
+  //       filteredDummyData.push(transaction);
+  //     }
+  //   });
 
-  console.log("Account ids:", accountIds);
+  //   console.log("Account ids:", accountIds);
 
-  // State to handle transactions
-  console.log("Type state", typeState);
-  const [transactions, setTransactions] = useState(filteredDummyData);
+  //   // State to handle transactions
+  //   console.log("Type state", typeState);
+  //   const [transactions, setTransactions] = useState(filteredDummyData);
 
   // Handling of global state change
-  const [transactionState, setTransactionState] = useState(false);
-  const updateTransactionState = () => {
-    setTransactionState(false);
-  };
+  //   const [transactionState, setTransactionState] = useState(false);
+  //   const updateTransactionState = () => {
+  //     setTransactionState(false);
+  //   };
 
   // Handling of delete modal state
   const [deleteModalOpen, setDeleteModalOpen] = useState(false);
