8268bf1660b876684209d17b978f515472aeb491
travis <traviscanbefound@gmail.com>
2022-12-03 13:24:40 +0800

    feat: skeleton transactions table

--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -1,45 +1,64 @@
 import { useState, useEffect } from "react";
-import Form from "react-bootstrap/Form";
-import { useNavigate, Outlet } from "react-router-dom";
-import AddButton from "./AddButton";
-import { Stack } from "react-bootstrap"
 
 const Home = () => {
-  const [account, setAccount] = useState("saving");
-  const navigate = useNavigate();
+  // Current dummy data
+  const dummyData = [
+    {
+      TransactionID: 1,
+      AccountID: 621156213,
+      ReceivingAccountID: 339657462,
+      Date: "2022-11-08T04:00:00.000Z",
+      TransactionAmount: 500.0,
+      Comment: "Monthly Pocket Money",
+    },
+    {
+      TransactionID: 2,
+      AccountID: 958945214,
+      ReceivingAccountID: 621156213,
+      Date: "2022-11-08T04:00:00.000Z",
+      TransactionAmount: 8996.0,
+      Comment: "School Fees",
+    },
+    {
+      TransactionID: 3,
+      AccountID: 828120424,
+      ReceivingAccountID: 322798030,
+      Date: "2022-11-25T04:00:00.000Z",
+      TransactionAmount: 3000.0,
+      Comment: "Driving Centre Top-up",
+    },
+    {
+      TransactionID: 4,
+      AccountID: 353677039,
+      ReceivingAccountID: 785703027,
+      Date: "2022-11-17T06:21:00.000Z",
+      TransactionAmount: 255.0,
+      Comment: "Tuition Fee Payment",
+    },
+    {
+      TransactionID: 5,
+      AccountID: 259555772,
+      ReceivingAccountID: 828120424,
+      Date: "2022-11-08T04:00:00.000Z",
+      TransactionAmount: 32.0,
+      Comment: "Books Payment",
+    },
+  ];
 
-  useEffect(() => {
-    navigate(`/${account}`);
-  }, [account]);
+  const [transactions, setTransactions] = useState(dummyData);
 
   return (
     <div>
-      <div style={{ marginTop: 40, display: "flex" }}>
-        <h1 style={{ width: "70%" }}>Welcome Vance</h1>
-        <Form.Select
-          aria-label="accounts"
-          value={account}
-          onChange={(e) => {
-            setAccount(e.target.value);
-          }}
-          size="sm"
-          style={{ width: "30%" }}
-        >
-          <option value="saving">Saving Account</option>
-          <option value="current">Current Account</option>
-          <option value="multiplier">Multiplier Account</option>
-        </Form.Select>
-      </div>
-      <div>
-        <Stack direction="horizontal" gap="2">
-          <div>
-            <Outlet />
-          </div>
-          <div className="ms-auto mt-4">
-            <AddButton />
-          </div>
-        </Stack>
-      </div>
+      {dummyData.map((transaction) => (
+        <li>
+          <h1>Transaction ID: {dummyData[0].TransactionID}</h1>
+          <h1>Account ID: {dummyData[0].AccountID}</h1>
+          <h1>Receiving Account ID: {dummyData[0].ReceivingAccountID}</h1>
+          <h1>Date: {dummyData[0].Date}</h1>
+          <h1>Transaction Amount: {dummyData[0].TransactionAmount}</h1>
+          <h1>Comment: {dummyData[0].Comment}</h1>
+        </li>
+      ))}
     </div>
   );
 };
