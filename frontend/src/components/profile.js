d31137e61d6968d3069044849756d428a1973b14
jiaengoh <57712490+jiaengoh@users.noreply.github.com>
2022-12-03 13:32:15 +0800

    feature/profile

--- frontend/src/components/profile.js
+++ frontend/src/components/profile.js
@@ -1,5 +1,28 @@
+import React from "react";
+import AuthService from "../services/auth";
+
 const Profile = () => {
-  return <></>;
+  const currentUser = AuthService.getCurrentUser();
+
+  return (
+    <div className="container">
+      <header className="jumbotron">
+        <h3>
+          <strong>Welcome, {currentUser.email}</strong>
+        </h3>
+      </header>
+      <p>
+        <strong>JWT Token: </strong>
+        {currentUser.Id}
+      </p>
+      <p>
+        <strong>ID: </strong> {currentUser.Id}
+      </p>
+      <p>
+        <strong>Email: </strong> {currentUser.Email}
+      </p>
+    </div>
+  );
 };
 
 export default Profile;
