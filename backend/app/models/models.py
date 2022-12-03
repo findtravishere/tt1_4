6238f276a5216cc8ff341bb377b4e2308dbd09ce
xyzhang00 <>
2022-12-03 13:29:31 +0800

    Update schema

--- backend/app/models/models.py
+++ backend/app/models/models.py
@@ -8,30 +8,30 @@ from sqlalchemy.sql.expression import text
 class User(Base):
     __tablename__ = "user"
     user_id = Column(Integer, primary_key=True, nullable=False)
-    username = Column(String(20), nullable = True)
-    password = Column(String(20), nullable = True)
-    first_name = Column(String(255), nullable = True)
-    last_name = Column(String(255), nullable = True)
-    email = Column(String(255), nullable=True)
-    address = Column(String(255), nullable=True)
-    opt_into_phy_statements = Column(Boolean, nullable=True)
+    username = Column(String(20), nullable = True, default = None)
+    password = Column(String(20), nullable = True, default = None)
+    first_name = Column(String(255), nullable = True, default = None)
+    last_name = Column(String(255), nullable = True, default = None)
+    email = Column(String(255), nullable=True, default = None)
+    address = Column(String(255), nullable=True, default = None)
+    opt_into_phy_statements = Column(Boolean, nullable=True, default = None)
 
     
 class BankAccount(Base):
     __tablename__ = "bank_account"
     account_id = Column(Integer, primary_key = True, nullable = False)
-    user_id = Column(Integer, ForeignKey("User.user_id"), primary_key = True, nullable = False, )
-    account_type = Column(String(255), nullable = True)
-    account_balance = Column(FLOAT(precision=10, scale=2), nullable = True)
+    user_id = Column(Integer, ForeignKey("User.user_id"), primary_key = True, nullable = False)
+    account_type = Column(String(255), nullable = True, default = None)
+    account_balance = Column(FLOAT(precision=10, scale=2), nullable = True, default = None)
     
     
 class ScheduledTransactions(Base):
     __tablename__ = "scheduled_transactions"
     transaction_id = Column(Integer, primary_key = True, nullable = False)
     account_id = Column(Integer, ForeignKey("User.user_id"), primary_key = True, nullable = False)
-    receiving_account_id = Column(Integer, nullable = True)
-    date = Column(String(255), nullable = True)
-    transaction_amount = Column(FLOAT(precision=10, scale=2), nullable = True)
-    comment = Column(String(255), nullable = True)
+    receiving_account_id = Column(Integer, nullable = True, default = None)
+    date = Column(String(255), nullable = True, default = None)
+    transaction_amount = Column(FLOAT(precision=10, scale=2), nullable = True, default = None)
+    comment = Column(String(255), nullable = True, default = None)
     
     
